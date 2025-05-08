1e934e934988591b39a6b286442a9be01332df71
ridouane-dev <ridouaneibrahim46@gmail.com>
2025-05-08 12:42:46 +0200

    check valider en locale

--- current_datetime.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# la date et l'heure sont afficher grace a cette consigne $(date "%Y-%m-%d %H:%M:%S
-#elle donne la date et l'heure sous le format yyyy-mm-dd  hh:mm:ss
-echo "date et heure actuelle : $(date '+%Y-%m-%d %H:%M:%S')"
