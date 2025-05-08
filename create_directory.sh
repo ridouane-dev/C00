77c9b10c7365650a51c517e7befcc99d8163a2e5
ridouane-dev <ridouaneibrahim46@gmail.com>
2025-05-08 11:27:10 +0200

    enfin

--- create_directory.sh
+++ create_directory.sh
@@ -2,6 +2,7 @@
 #creation d'une variable pour nomer le dossier
 # demande a l'utilisateur d enter le nom de son repertoire
 
-read -p "entrer le nom de votre repertoire : " mon_repertoire
 
-   mkdir "$mon_repertoire"
+
+
+mkdir test_directory
