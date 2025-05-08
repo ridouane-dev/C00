f9a91e8a1450b50274da843e9d72003d9ba430ab
ridouane-dev <ridouaneibrahim46@gmail.com>
2025-05-08 12:44:18 +0200

    check valider en locale

---
+++ hello_world.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#la commamde echo permet d'afficher des donnee c  comme printf en c 
+#d'apres ce que j`ai compirs
+
+echo "Hello, World!"
---
+++ list_files.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "liste des fichiers"
+
+ls -l
