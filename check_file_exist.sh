45640513ddf4345260763409826b56a7e2a4bc94
ridouane-dev <ridouaneibrahim46@gmail.com>
2025-05-08 13:39:35 +0200

    check valider en locale

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -7,7 +7,7 @@ read -p "Entrez le nom du fichier : " filename
 #si le fichier filename existe alors i affichera le message suivans
 if [ -f"$filename" ]; then
 
-  echo "Le fichier '$filename'  existe"
+  echo "Le fichier '$filename' sexiste"
 
 #dans le cas contriare il affichera le message suivants
 else
