d14bf0656b3ee34933b9898848c6994f3a727a6a
ridouane-dev <ridouaneibrahim46@gmail.com>
2025-05-08 10:44:39 +0200

    commite de correction

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #commande pour demander le nom du fichier 
 
-read -p "entrer le nom du fichier que vous chercher : " file
+read -p "entrer le nom du fichier que vous chercher : " filename
 
 #condition pour verifier si le fichier eexiste
-#si le fichier file existe alors 
-if [ -f "$file"]; then
-  echo "le fichier '$file'  existe"
+#si le fichier filename existe alors 
+if [ -f "$filename"]; then
+  echo "le fichier '$filename'  existe"
 
 #si le ficher n'existe pas alors
 else
-  echo "le fichier '$file' n'existe pas "
+  echo "le fichier '$filename' n'existe pas "
 fi
--- hello_world.sh
+++ hello_world.sh
@@ -3,4 +3,4 @@
 #la commamde echo permet d'afficher des donnee c  comme printf en c 
 #d'apres ce que j`ai compirs
 
-echo "hello, world!"
+echo "Hello, World!"
