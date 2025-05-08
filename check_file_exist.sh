76332ff5f22271f2ea5cb10b86cf6ce6c7baf97c
ridouane-dev <ridouaneibrahim46@gmail.com>
2025-05-08 16:01:19 +0200

    kkk

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -2,14 +2,14 @@
 #commande pour demander le nom du fichier 
 
 read -p "Entrez le nom du fichier : " filename
-
 #condition pour verifier si le fichier eexiste
 #si le fichier filename existe alors i affichera le message suivans
 if [ -f $filename ]; then
 
-echo "Le fichier '$filename' existe."
-
+echo "
+Le fichier '$filename' existe."
 #dans le cas contriare il affichera le message suivants
 else
-echo "Le fichier '$filename' n'existe pas."
+echo "
+Le fichier '$filename' n'existe pas."
 fi
