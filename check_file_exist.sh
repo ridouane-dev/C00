133a8f298bcab33d9474666081cb847d70a3e9f6
ridouane-dev <ridouaneibrahim46@gmail.com>
2025-05-08 10:59:26 +0200

    correction check_file_exixt.sh

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -5,7 +5,7 @@ read -p "entrer le nom du fichier que vous chercher : " filename
 
 #condition pour verifier si le fichier eexiste
 #si le fichier filename existe alors 
-if [ -f "$filename"]; then
+if [ -f $filename ]; then
   echo "le fichier '$filename'  existe"
 
 #si le ficher n'existe pas alors
