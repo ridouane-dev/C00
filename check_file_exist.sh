763e7be1aca4cdebe05b8f3c1c79682d1b15bd1d
ridouane-dev <ridouaneibrahim46@gmail.com>
2025-05-08 18:13:38 +0200

    correction de tout les fichier plus ajout de nouveaux commentaire

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #commande pour demander le nom du fichier 
 
-read -p "Entrez le nom du fichier :" filename
+read -p "Entrez le nom du fichier : "   filename
 #condition pour verifier si le fichier eexiste
 #si le fichier filename existe alors i affichera le message suivans
 if [ -f $filename ]; then
