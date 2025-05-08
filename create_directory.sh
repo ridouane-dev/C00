1da2ae4bc609d239cc5d1bb4179abf817e02a283
ridouane-dev <ridouaneibrahim46@gmail.com>
2025-05-08 11:16:27 +0200

    converting this fucking sheat

--- create_directory.sh
+++ create_directory.sh
@@ -4,16 +4,4 @@
 
 read -p "entrer le nom de votre repertoire : " mon_repertoire
 
-#creation d'une condition pour verifier si le repertoir existe deja 
-#verification si le repertoire test_directory existe
-
-if [ -d "$mon_repertoire" ]; then 
-	echo "le repertoir '$mon_repertoire' existe deja."
-#si il n'existe pas creer un avec la commande mkdir et afficher un message 
-#de success
-
-else
    mkdir "$mon_repertoire"
-     echo "le repertoire  '$mon_repertoire'a ete creer avec succes"
-
-fi
